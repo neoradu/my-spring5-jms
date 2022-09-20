8a5ef10430565a14861ffa720049521058faafe6
Radu Radu <neoradu@gmail.com>
2022-09-20 13:08:16 +0300

    sending and receiving JMS messages through ActiveMq running on Docker
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- src/main/java/radu/learn/jmsdemo/JmsdemoApplication.java
+++ src/main/java/radu/learn/jmsdemo/JmsdemoApplication.java
@@ -1,8 +1,5 @@
 package radu.learn.jmsdemo;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JmsdemoApplication {
 
 	public static void main(String[] args) throws Exception {
-		//minimum ActiveMq server for a hello world
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(
+		//THis is not necesarry. If we have the dependencies in the pom, Spring boot will runs an embedded server automatically
+		/*ActiveMQServer server = ActiveMQServers.newActiveMQServer(
 				                                  new ConfigurationImpl()
 				                                  	  .setPersistenceEnabled(false)
 				                                  	  .setJournalDirectory("target/data/journal")
 				                                  	  .setSecurityEnabled(false)
 				                                  	  .addAcceptorConfiguration("invm", "vm://0"));
-		server.start();
+		server.start();*/
 		SpringApplication.run(JmsdemoApplication.class, args);
 	}
 
